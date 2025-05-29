db238f601020145143332c77167915e3798617bd
Ivan Tulaev <ivan.aks.msk@gmail.com>
2025-05-29 15:07:30 +0300

    chore: add and config  @html-eslint/eslint-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'eslint/config';
 
 import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
+import html from "@html-eslint/eslint-plugin";
 
 export default defineConfig([
   {
     ignores: [ 'node_modules/**/*', 'dist/**/*', 'example/dist/**/*' ],
+  },
+  {
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"],
+    rules: {
+      ...html.configs["flat/recommended"].rules, // Must be defined. If not, all recommended rules will be lost
+      "@html-eslint/indent": ["error", 2],
+    },
   },
   ...bpmnIoPlugin.configs.browser.map(config => {
     return {
