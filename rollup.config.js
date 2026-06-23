2ed0d317ee1f5bd580cabba9d3d6312e49271306
Wenish <Wenish@users.noreply.github.com>
2026-06-23 15:48:59 +0200

    feat: update package configuration for UMD and IIFE formats

--- rollup.config.js
+++ rollup.config.js
@@ -3,25 +3,51 @@ import resolve from '@rollup/plugin-node-resolve';
 import fs from 'fs';
 const pkg = importPkg();
 
-export default {
-  input: 'lib/index.js',
-  output: [
-    {
+export default [
+  {
+    input: 'lib/index.js',
+    output: [
+      {
+        sourcemap: true,
+        format: 'esm',
+        file: pkg.exports['.'].import,
+      },
+      {
+        sourcemap: true,
+        format: 'cjs',
+        file: pkg.exports['.'].require,
+      }
+    ],
+    external: Object.keys(pkg.dependencies),
+    plugins: [
+      resolve()
+    ]
+  },
+  {
+    input: 'lib/index.js',
+    output: {
       sourcemap: true,
-      format: 'esm',
-      file: pkg.exports['.'].import,
+      format: 'umd',
+      name: 'BpmnAutoLayout',
+      file: 'dist/bpmn-auto-layout.umd.js',
     },
-    {
+    plugins: [
+      resolve()
+    ]
+  },
+  {
+    input: 'lib/index.js',
+    output: {
       sourcemap: true,
-      format: 'cjs',
-      file: pkg.exports['.'].require,
-    }
-  ],
-  external: Object.keys(pkg.dependencies),
-  plugins: [
-    resolve()
-  ]
-};
+      format: 'iife',
+      name: 'bpmnAutoLayout',
+      file: 'dist/bpmn-auto-layout.iife.js',
+    },
+    plugins: [
+      resolve()
+    ]
+  }
+];
 
 function importPkg() {
   return JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf8' }));
